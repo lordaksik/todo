739db6acc84b98b257d6456345477109db1c8392
lordaksik <lordaks@tut.by>
2024-12-23 18:29:36 +0300

    Начальный todo list

--- src/App.js
+++ src/App.js
@@ -1,26 +1,53 @@
 import './App.css';
+import {useState} from "react";
 
+const lists = [];
 var nameTable = 'Список задач'
+
+function ElementList({todos}) {
+    const liElement = todos.map(list => (
+        <li key={list.id}>
+            {list.text}
+        </li>
+    ))
+    return (<ol>{liElement}</ol>)
+}
+
+function ButtonAddElemnt({setText}, {setTodos}, {todos}, {text}) {
+    return (<button onClick={() => {
+        setText('');
+        setTodos([{
+            id: todos.length,
+            text: text
+        }, ...todos]);
+    }} className='inputFieldButton'>Создать
+    </button>)
+}
+
 export default function App() {
+
+    const [todos, setTodos] = useState(lists);
+    const [text, setText] = useState('');
+
     return (
         <div className='toDoList'>
             <h1>{nameTable}</h1>
-            <ol>
-                <ListName/>
-            </ol>
+            <div className='addInList'><input value={text}
+                                              onChange={e => setText(e.target.value)}
+                                              className='inputField' type='text'/>
+                <button onClick={() => {
+                    setText('');
+                    setTodos([{
+                        id: todos.length,
+                        text: text
+                    }, ...todos]);
+                }} className='inputFieldButton'>Создать
+                </button>
+            </div>
+            <ElementList todos={todos}/>
         </div>
     );
 }
 
-//  <MyButton info='правду'/>
-function ListName() {
-    var list = {
-        id: 1,
-        title: 'Сделать сегодня'
-    }
-    return (
-        <li key={list.id}>{list.title} </li>
-    );
-}
 
 
