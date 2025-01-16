ba03bcf97f62ca17093efbd0ade96579337f0804
lordaksik <lordaks@tut.by>
2025-01-16 18:56:37 +0300

    toDoList 1 вариант

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ const lists = [{
     id: 0,
     text: 'Планы на завтра'
 }];
+
 var nameTable = 'Список задач'
 
 export default function App() {
@@ -21,31 +22,66 @@ export default function App() {
         );
     }
 
+
     function ElementList({todos}) {
+
         const liElement = todos.map(list => (
             <li key={list.id}>
-                {list.text}
-                <span className='elementButton'>
-                <button className='editElement'>
-             <img src={editIkon}></img>
-                </button>
-                <button className='deleteElement' onClick={() => deleteElement(list.id)}>
-                    <img src={deleteIkon}></img>
-                </button>
-            </span>
+                <ElementButton element={list}/>
             </li>
         ))
         return (<ol reversed>{liElement}</ol>)
     }
 
+    function ElementButton({element}) {
+        const [isEditing, setIsEditing] = useState(false);
+        const [textLi, setTextLi] = useState(element.text);
+        let todoContent, liText;
+
+        if (isEditing) {
+            liText = (
+                <input value={textLi}
+                       onChange={e => setTextLi(e.target.value)}
+                       className='inputLiElement' type='text'/>
+            )
+            todoContent = (
+                <button className='editElement' onClick={() => {
+                    setIsEditing(false)
+                    element.text = textLi
+                }
+                }>
+                    Сохранить
+                </button>
+            );
+
+        } else {
+            todoContent = (
+                <button className='editElement' onClick={() => setIsEditing(true)}>
+                    <img alt='edit Ikon' src={editIkon}></img>
+                </button>
+            );
+            liText = textLi
+        }
+        return (
+            <>
+                {liText}
+                <span className='elementButton'>
+                {todoContent}
+                    <button className='deleteElement' onClick={() => deleteElement(element.id)}>
+                    <img alt='delete Ikon' src={deleteIkon}></img>
+                </button>
+            </span>
+            </>
+        );
+    }
+
     return (
         <div className='toDoList'>
             <h1>{nameTable}</h1>
-            <div className='addInList'><input value={text}
+            <div className='addInList'><input alt='' value={text}
                                               onChange={e => setText(e.target.value)}
                                               className='inputField' type='text'/>
-                <button onClick={() => {
-                    console.log(todos)
+                <button  onClick={() => {
                     setText('');
                     setTodos([{
                         id: nextId++,
